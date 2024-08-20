878739d9b40d61f0cc42b469c1f2d68d6113ed29
fchinu <fachi2001@gmail.com>
2024-08-20 11:36:13 +0200

    Add centrality selections for projections

--- ProjectDataFromSparse.py
+++ ProjectDataFromSparse.py
@@ -17,6 +17,7 @@ parser.add_argument('cutSetFileName', metavar='text', default='cutSetFileName.ym
                     help='input file with cut set')
 parser.add_argument('outFileName', metavar='text', default='outFileName.root',
                     help='output root file name')
+parser.add_argument('--isMC', action='store_true', help='is MC data', required=False, default=False)
 args = parser.parse_args()
 
 
@@ -27,11 +28,27 @@ with open(args.cutSetFileName, 'r') as ymlCutSetFile:
 # Open the file
 file = TFile.Open(args.sparseName)
 # Get the THnSparse
-sparse = file.Get("hf-task-ds/hSparseMass")
+if args.isMC:
+    sparse = file.Get("hf-task-ds/MC/Dplus/Prompt/hSparseMass")
+else:
+    try:
+        sparse = file.Get("hf-task-ds/hSparseMass") # Old file structure
+        sparse.GetAxis(0)
+    except:
+        sparse = file.Get("hf-task-ds/Data/hSparseMass")
+        sparse.GetAxis(0)
+
+hEv = file.Get("bc-selection-task/hCounterTVXafterBCcuts")
+hEv.SetDirectory(0)
+
+if "Cent" in cutSetCfg['cutvars']:
+    centralitySelections = cutSetCfg['cutvars'].pop('Cent')
+    print(f"Centrality selections: {centralitySelections}")
 
 # Extract variable names and cuts from the config
 var_names = list(cutSetCfg['cutvars'].keys())
 var_names.remove('InvMass')
+
 cuts = [list(zip(cutSetCfg['cutvars'][var]['min'], cutSetCfg['cutvars'][var]['max'])) for var in var_names]
 axes = [cutSetCfg['cutvars'][var]['axisnum'] for var in var_names]
 
@@ -41,6 +58,24 @@ hPts = []
 for iPt in range(len(cutSetCfg['cutvars']['Pt']['min'])):
     ptLowLabel = cutSetCfg['cutvars']['Pt']['min'][iPt] * 10
     ptHighLabel = cutSetCfg['cutvars']['Pt']['max'][iPt] * 10
+    if centralitySelections:
+        for iCent in range(len(centralitySelections['min'])):
+            centLowLabel = centralitySelections['min'][iCent]
+            centHighLabel = centralitySelections['max'][iCent]
+            sparse.GetAxis(centralitySelections['axisnum']).SetRangeUser(centLowLabel, centHighLabel)
+            for i, axis in enumerate(axes):
+                min = cuts[i][iPt][0]
+                max = cuts[i][iPt][1]
+                sparse.GetAxis(axis).SetRangeUser(min, max)
+            hM = sparse.Projection(0)
+            hM.SetDirectory(0)
+            hPt = sparse.Projection(1)
+            hPt.SetDirectory(0)
+            hM.SetName(f'hMass_{ptLowLabel:.0f}_{ptHighLabel:.0f}_Cent_{centLowLabel:.0f}_{centHighLabel:.0f}')
+            hPt.SetName(f'hPt_{ptLowLabel:.0f}_{ptHighLabel:.0f}_Cent_{centLowLabel:.0f}_{centHighLabel:.0f}')
+            hMs.append(hM)
+            hPts.append(hPt)
+            
     for i, axis in enumerate(axes):
         min = cuts[i][iPt][0]
         max = cuts[i][iPt][1]
@@ -60,4 +95,5 @@ for hM in hMs:
     hM.Write()
 for hPt in hPts:
     hPt.Write()
+hEv.Write("hEv")
 outFile.Close()
